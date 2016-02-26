36a6829899901f4305443b91409cb74dce8d3be1
phddone <superdriver405@gmail.com>
2016-02-25 23:07:44 -0800

    list items in table, add sort by title and price drop

--- js/main.js
+++ js/main.js
@@ -6,6 +6,96 @@ $(function () {
     });
 });
 
+(function () {
+    // Private function
+    function getColumnsForScaffolding(data) {
+        if ((typeof data.length !== 'number') || data.length === 0) {
+            return [];
+        }
+        var columns = [];
+        for (var propertyName in data[0]) {
+            columns.push({ headerText: propertyName, rowText: propertyName });
+        }
+        return columns;
+    }
+
+    ko.simpleGrid = {
+        // Defines a view model class you can use to populate a grid
+        viewModel: function (configuration) {
+            this.data = configuration.data;
+            this.currentPageIndex = ko.observable(0);
+            this.pageSize = configuration.pageSize || 5;
+
+            // If you don't specify columns configuration, we'll use scaffolding
+            this.columns = configuration.columns || getColumnsForScaffolding(ko.unwrap(this.data));
+
+            this.itemsOnCurrentPage = ko.computed(function () {
+                var startIndex = this.pageSize * this.currentPageIndex();
+                return ko.unwrap(this.data).slice(startIndex, startIndex + this.pageSize);
+            }, this);
+
+            this.maxPageIndex = ko.computed(function () {
+                return Math.ceil(ko.unwrap(this.data).length / this.pageSize) - 1;
+            }, this);
+        }
+    };
+
+    // Templates used to render the grid
+    var templateEngine = new ko.nativeTemplateEngine();
+
+    templateEngine.addTemplate = function(templateName, templateMarkup) {
+        document.write("<script type='text/html' id='" + templateName + "'>" + templateMarkup + "<" + "/script>");
+    };
+
+    templateEngine.addTemplate("ko_simpleGrid_grid", "\
+                    <table class=\"ko-grid\" cellspacing=\"0\">\
+                        <thead>\
+                            <tr data-bind=\"foreach: columns\">\
+                               <th data-bind=\"text: headerText\"></th>\
+                            </tr>\
+                        </thead>\
+                        <tbody data-bind=\"foreach: itemsOnCurrentPage\">\
+                           <tr data-bind=\"foreach: $parent.columns\">\
+                               <td data-bind=\"text: typeof rowText == 'function' ? rowText($parent) : $parent[rowText] \"></td>\
+                            </tr>\
+                        </tbody>\
+                    </table>");
+    templateEngine.addTemplate("ko_simpleGrid_pageLinks", "\
+                    <div class=\"ko-grid-pageLinks\">\
+                        <span>Page:</span>\
+                        <!-- ko foreach: ko.utils.range(0, maxPageIndex) -->\
+                               <a href=\"#\" data-bind=\"text: $data + 1, click: function() { $root.currentPageIndex($data) }, css: { selected: $data == $root.currentPageIndex() }\">\
+                            </a>\
+                        <!-- /ko -->\
+                    </div>");
+
+    // The "simpleGrid" binding
+    ko.bindingHandlers.simpleGrid = {
+        init: function() {
+            return { 'controlsDescendantBindings': true };
+        },
+        // This method is called to initialize the node, and will also be called again if you change what the grid is bound to
+        update: function (element, viewModelAccessor, allBindings) {
+            var viewModel = viewModelAccessor();
+
+            // Empty the element
+            while(element.firstChild)
+                ko.removeNode(element.firstChild);
+
+            // Allow the default templates to be overridden
+            var gridTemplateName      = allBindings.get('simpleGridTemplate') || "ko_simpleGrid_grid",
+                pageLinksTemplateName = allBindings.get('simpleGridPagerTemplate') || "ko_simpleGrid_pageLinks";
+
+            // Render the main grid
+            var gridContainer = element.appendChild(document.createElement("DIV"));
+            ko.renderTemplate(gridTemplateName, viewModel, { templateEngine: templateEngine }, gridContainer, "replaceNode");
+
+            // Render the page links
+            var pageLinksContainer = element.appendChild(document.createElement("DIV"));
+            ko.renderTemplate(pageLinksTemplateName, viewModel, { templateEngine: templateEngine }, pageLinksContainer, "replaceNode");
+        }
+    };
+})();
 
 /* Demo data */
 var demoData = [
@@ -13,8 +103,8 @@ var demoData = [
         id: 1,
         title: 'Laptop 1',
         url: 'www.bestbuy.com',
-        price: 90,
-        drop: 10,
+        priceOld: 100,
+        priceNew: 90,
         head: 1,
         store: 'bestbuy'
     },
@@ -22,8 +112,8 @@ var demoData = [
         id: 2,
         title: 'Laptop 2',
         url: 'www.bestbuy.com',
-        price: 90,
-        drop: 10,
+        priceOld: 100,
+        priceNew: 90,
         head: 1,
         store: 'bestbuy'
     },
@@ -31,8 +121,8 @@ var demoData = [
         id: 3,
         title: 'Laptop 3',
         url: 'www.staples.com',
-        price: 90,
-        drop: 10,
+        priceOld: 100,
+        priceNew: 90,
         head: 1,
         store: 'staples'
     },
@@ -40,8 +130,8 @@ var demoData = [
         id: 4,
         title: 'Laptop 4',
         url: 'www.staples.com',
-        price: 100,
-        drop: 10,
+        priceOld: 120,
+        priceNew: 90,
         head: 1,
         store: 'staples'
     },
@@ -49,8 +139,8 @@ var demoData = [
         id: 5,
         title: 'Laptop 5',
         url: 'www.ms.com',
-        price: 100,
-        drop: 10,
+        priceOld: 110,
+        priceNew: 90,
         head: 1,
         store: 'microsoft'
     }
@@ -62,8 +152,8 @@ var generateData = function () {
             id: 1,
             title: 'Laptop' + i,
             url: 'www.bestbuy.com',
-            price: 90,
-            drop: i,
+            priceOld: 90 + i,
+            priceNew: 110 - i,
             head: 1,
             store: 'bestbuy'
         });
@@ -91,15 +181,30 @@ var appViewModel = function () {
     self.sortDirection = ko.observable(defaultDirection); // By default, sorting in descending direction
 
     var Initialize = function () {
+        self.gridViewModel = new ko.simpleGrid.viewModel({
+            data: self.items,
+            columns: [
+                { headerText: "Title", rowText: "title" },
+                { headerText: "Url", rowText: "url"},
+                { headerText: "Old Price", rowText: "priceOld" },
+                { headerText: "New Price", rowText: "priceNew" },
+                { headerText: "Price Drop", rowText: function (item) {
+                    return item.priceOld - item.priceNew } },
+            ],
+            pageSize: 10
+        });
+
         self.searchItemsByStore('bestbuy');
+        self.jumpToFirstPage();
 
     };
+
     var Item = function (item) {
         var self = this;
         self.title = item.title;
         self.url = item.url;
-        self.price = item.price;
-        self.drop = item.drop;
+        self.priceOld = item.priceOld;
+        self.priceNew = item.priceNew;
         self.store = item.store;
     };
 
@@ -125,6 +230,7 @@ var appViewModel = function () {
 
     self.searchItemsByTitle = function () {
         self.setItemsByTitle();
+        self.jumpToFirstPage();
     };
 
     self.getStoreId = function () {
@@ -198,6 +304,25 @@ var appViewModel = function () {
             self.items.sort(self.sortItemsList);
         }
 
+    };
+
+
+    self.sortByTitle = function() {
+        self.items.sort(function(a, b) {
+            return a.title < b.title ? -1 : 1;
+        });
+    };
+
+    self.sortByDrop = function() {
+        self.items.sort(function(a, b) {
+            var dropa = a.priceOld - a.priceNew;
+            var dropb = b.priceOld - b.priceNew;
+            return dropa > dropb ? -1 : 1;
+        });
+    };
+
+    self.jumpToFirstPage = function() {
+            self.gridViewModel.currentPageIndex(0);
     };
 
     generateData();
